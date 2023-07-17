1417bd830ab5b2e802a86d54c195a4ac83533b4f
halil muslubas <halil_muslubas@hotmail.com>
2023-07-17 19:18:45 +0300

    yeni eklemeler 17.07_19

--- firma.py
+++ firma.py
@@ -9,8 +9,10 @@ class Firma:
         self.satis_sayisi=satis_sayisi
         self.toplamSatis=toplamSatis
 class Firma_islemleri:
+    
     def kod_olustur(self):
         return random.randint(10000,99999)
+    
     def firma_kaydet(self):
         strg=[]
         dosya_adi="firmalar.csv"
@@ -19,7 +21,7 @@ class Firma_islemleri:
                 strg.append(f"{frm.id};{frm.firmaAdi};{frm.puan}\n")
             with open(dosya_adi,"w",encoding="utf-8") as fl:
                 fl.writelines(strg)
-                
+    
     def firma_ekle(self):
         if not Firma.firmalar:self.firma_oku()
         frm_kod=self.kod_olustur()
@@ -54,4 +56,33 @@ class Firma_islemleri:
                     #satis_sayisi,toplam_satis=self.satis_hesapla(int(id))
                     firma=Firma(int(id),adi,int(puan))
                     Firma.firmalar.append(firma)
-            
+    def auto_firma(self):
+        for i in range(1,10):
+            frm_kod=self.kod_olustur()
+            frm_adi=f"{i}. Firma"
+            frm_rnd=random.randint(1,100)
+            firma=Firma(frm_kod,frm_adi,frm_rnd)
+            Firma.firmalar.append(firma)
+        self.firma_kaydet()
+        
+    def firma_menu(self):
+            while True:
+                print("*******************************************************")
+                print("** 1 - Firma Ekle____________________________________**")
+                print("** 2 - Firma Sil_____________________________________**")
+                print("** 3 - Firma Listele_________________________________**")
+                print("** 4 - Auto Firma Ekle_______________________________**")
+                print("** 5 - Çıkış_________________________________________**")
+                print("*******************************************************")
+                secim=input("Seçimizniz:")
+                if secim=="1":
+                    self.firma_ekle()
+                elif secim=="2":
+                    mst_id=input("Firma Kodunu Giriniz:")
+                    self.firma_sil(mst_id)
+                elif secim=="3":
+                    self.firma_listele()
+                elif secim=="4":
+                    self.auto_firma()
+                elif secim=="5":
+                    break
--- musteri.py
+++ musteri.py
@@ -1,13 +1,115 @@
+import random
+import datetime
 class Musteri:
-    musteriler=[]
-    def __init__(self,musteri_kodu,musteri_Adi,musteri_cinsiyet,musteri_yas,musteri_Puan,musteri_islem_sayisi=0,musteri_alis=0):
+    musteriler=[]   
+    def __init__(self,musteri_kodu,musteri_adi,musteri_soyadi,musteri_cinsiyet,musteri_yas,musteri_Puan,kayit_tarihi):
         self.musteri_kodu=musteri_kodu
-        self.musteri_Adi=musteri_Adi
+        self.musteri_adi=musteri_adi
+        self.musteri_soyadi=musteri_soyadi
         self.musteri_cinsiyet=musteri_cinsiyet
         self.musteri_yas=musteri_yas
         self.musteri_Puan=musteri_Puan
-        self.musteri_islem_sayisi=musteri_islem_sayisi
-        self.musteri_alis=musteri_alis
+        self.kayit_tarihi=kayit_tarihi
 
 class Musteri_islemleri:
-    pass
+
+    def musteri_oku(self):
+        Musteri.musteriler.clear
+        dosya_adi="musteri.csv"
+        with open(dosya_adi,"r",encoding="utf-8") as fl:
+            for satir in fl:
+                satir=satir.strip()
+                if satir:
+                    musteri_kodu, musteri_Adi,musteri_soyadi,musteri_cinsiyet,musteri_yas, musteri_Puan,kayit_tarihi  =satir.split(";")
+                    musteri=Musteri(musteri_kodu, musteri_Adi,musteri_soyadi,musteri_cinsiyet,musteri_yas, musteri_Puan,kayit_tarihi)
+                    Musteri.musteriler.append(musteri)
+                    
+    def musteri_kaydet(self):
+        dosya_adi="musteri.csv"
+        strg=[]
+        if Musteri.musteriler:
+            for mst in Musteri.musteriler:
+                strg.append(f"{mst.musteri_kodu};{mst.musteri_adi};{mst.musteri_soyadi};{mst.musteri_cinsiyet};{mst.musteri_yas};{mst.musteri_Puan};{mst.kayit_tarihi}\n")
+            
+            with open(dosya_adi,"w",encoding="utf-8") as fl:
+                fl.writelines(strg)
+        #self.musteri_oku()
+
+    def kod_olustur(self):
+        return random.randint(10000,99999)
+    
+    def musteri_giris(self):
+        musteri_kodu=self.kod_olustur()
+        musteri_adi=input("Adınızı Giriniz:")
+        musteri_soyadı=input("Soyadınızı Giriniz:")
+        musteri_cinsiyet=input("Cinsiyetinizi Giriniz")
+        musteri_yas=input("Yaşınızı Giriniz")
+        musteri_puan=random.randint(1,100)
+        kayit_tarihi=datetime.datetime().date()
+        musteri=Musteri(musteri_kodu,musteri_adi,musteri_soyadı,musteri_cinsiyet,musteri_yas,musteri_puan,kayit_tarihi)
+        self.musteri_ekle(musteri)
+        
+    def auto_musteri(self):
+        isimler=['Ahmet', 'Mehmet', 'Ali', 'Mustafa', 'Emre', 'İbrahim', 'Hasan', 'Yusuf', 'Ömer', 'Serkan',
+            'Ayşe', 'Fatma', 'Zeynep', 'Emine', 'Hatice', 'Esra', 'İrem', 'Aylin', 'Elif', 'Sedef','Deniz', 'Yağmur', 'Cem', 'Ege', 'Can', 'İpek', 'Ceren', 'Aslı', 'Doruk', 'Derin']
+        soyadlari = ['Yılmaz', 'Demir', 'Kaya', 'Çelik', 'Aydın', 'Şahin', 'Aksoy', 'Koç', 'Öztürk', 'Can']
+        
+        for i in range(0,random.randint(1,10)):
+            kod=self.kod_olustur()
+            ad=random.choice(isimler)
+            soyad=random.choice(soyadlari)
+            cns=random.choice("EK")
+            yas=random.randint(20,50)
+            puan=random.randint(1,100)
+            tarih=datetime.datetime.now().date()
+            while any(musteri.musteri_kodu == kod for musteri in Musteri.musteriler):
+                kod = self.kod_olustur()
+            musteri=Musteri(kod,ad,soyad,cns,yas,puan,tarih)
+            
+        self.musteri_ekle(musteri)
+            
+    def musteri_ekle(self,musteri):
+        Musteri.musteriler.append(musteri)
+        self.musteri_kaydet()
+    
+    def musteri_sil(self,mst_id):
+        if Musteri.musteriler:
+            for mst in Musteri.musteriler:
+                if mst_id==mst.musteri_kodu:
+                    Musteri.musteriler.remove(mst)
+                    break
+            self.musteri_kaydet()
+            
+    def musteri_listele(self):
+        if Musteri.musteriler:
+            print("Müşteri Listesi")
+            print("Kodu     Adı Soyadı     Cns  Yaş     Puan    K.Tarihi")
+            print("******************************************************")
+            for mst in Musteri.musteriler:
+                print(f"* | {mst.musteri_kodu}| {mst.musteri_adi} {mst.musteri_soyadi} | {mst.musteri_cinsiyet} | {mst.musteri_yas} | {mst.musteri_Puan} | {mst.kayit_tarihi} |")
+            print("*******************************************************")
+            print(f"{len(Musteri.musteriler)} Adet Müşteri Listelendi")
+    def musteri_menu(self):
+        while True:
+            print("*******************************************************")
+            print("** 1 - Müşteri Ekle__________________________________**")
+            print("** 2 - Müşteri Sil___________________________________**")
+            print("** 3 - Müşteri Listele_______________________________**")
+            print("** 4 - Auto Müşteri Ekle_____________________________**")
+            print("** 5 - Çıkış_________________________________________**")
+            print("*******************************************************")
+            secim=input("Seçimizniz:")
+            if secim=="1":
+                self.musteri_ekle()
+            elif secim=="2":
+                mst_id=input("Müşteri Kodunu Giriniz:")
+                self.musteri_sil(mst_id)
+            elif secim=="3":
+                self.musteri_listele()
+            elif secim=="4":
+                self.auto_musteri()
+            elif secim=="5":
+                break
+
+m=Musteri_islemleri()
+m.musteri_menu() 
